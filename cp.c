502eb291cc8b243b596dc283ba114ead102225bd
James Porcelli <james.porcelli@stonybrook.edu>
2014-02-24 10:04:37 -0500

    Updated old school commenting scheme

--- cp.c
+++ cp.c
@@ -1,11 +1,13 @@
-/* James C. Porcelli, SBU-ID 108900819, CSE230,
-   Spring 2013, Prof. Esmaili, HW # 3, gcc - 4.6 */
+/* 
+@author - James C. Porcelli 
+@file - cp.c
+@app - Mancala, cmd line
+*/
 
 #include "mancala_game.h"
 
 /* Cp decision engine for random mode. */
 int cp_random(int rand_num, int num_pits){
-  /* RETURNS A RANDOMIZED PIT CHOICE BETWEEN [1, MAX_PITS] */
   return (1 + (int)( (num_pits * (rand_num / (RAND_MAX + 1.0)))));
 }
 
@@ -18,38 +20,18 @@ int cp_random(int rand_num, int num_pits){
 */
 int cp_hoarding(int rand_num, int num_pits, int playerA_pits[]){
   
-  /* WE ONLY USE THE RANDOM NUMBER NOW TO CHOOSE
-     THE PIT WE WILL HOARD IN. THIS IS DONE SO PLAYER B
-     DOESNT GET WISE TO OUR SCHEME AND COULD THEREFORE
-     MORE EASILY FORMULATE A COUNTER ATTACK WHERE BY HE
-     GAINS CONTROL OF OUR HOARDING PIT ! */
   int hoard_in = 1 + (int)( (num_pits * (rand_num / (RAND_MAX + 1.0))));
   
-  /* RETURN OUR PIT CHOICE AS ANY PIT EXCEPT THE HOARDING PIT UNLESS
-     WE MUST */
-  
-  /* CHECK IF WE WILL BE FORCED TO PICK OUR HOARDING PIT */
-  int marblesLeft = EMPTY, 
-    
-    /* COUNTER */
-    i,
-    
-    /* PIT CHOICE (BESIDES THE HOARDING PIT) */
-    pit_choice;
+  int marblesLeft = EMPTY, i, pit_choice;
   
   for(i = STARTING_PIT + 1;i < num_pits + 1;i++)
-    /* ADD ALL MARBLES LEFT IN OUR OTHER PITS */
     if(i != hoard_in)
       marblesLeft += playerA_pits[i];
   
-  /* WE HAVE NO CHOICE BUT TO PLAY OUR HOARDING PIT */
   if(marblesLeft == EMPTY)
     return hoard_in;
-  else 
-    /* RETURN A NON EMPTY PIT THATS NOT OUR HOARDING PIT */
-    {
+  else{
       for(i = STARTING_PIT + 1;i < num_pits + 1;i++)
-	/* THE FIRST NON EMPTY NON SCORING PIT WILL DO */
 	if(playerA_pits[i] != EMPTY && i!= hoard_in)
 	  return i;
     }
